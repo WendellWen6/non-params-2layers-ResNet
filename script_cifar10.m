76383d755ea93100db27c5dbd2a4f41d226b76c2
WendellWen6 <60184910+WendellWen6@users.noreply.github.com>
2022-03-21 21:55:31 +0000

    exp cifar-10 with smaller size

--- script_cifar10.m
+++ script_cifar10.m
@@ -3,28 +3,23 @@
 %load("cifar10data",'X_train','Y_train','X_test','Y_test');
 
 %smaller size
+clear;
 load('cifar10data','X_st','Y_st','X_stest','Y_stest');
 
 
 %set hyperparams for trainning
 T_gt = 1;
-trials = 1;
+trials = 2;
 
 %[d, n] = size(X_train);
 [d, n] = size(X_st);
 
-A_errs_lp = zeros(T_gt * trials, 1);
-B_errs_lp = zeros(T_gt * trials, 1);
 Y_errs_lp = zeros(T_gt * trials, 1);
-
-A_errs_bp = zeros(T_gt * trials, 1);
-B_errs_bp = zeros(T_gt * trials, 1);
 Y_errs_bp = zeros(T_gt * trials, 1);
 
 i = 1;
 
 for T_gt_it = 1 : T_gt
-  [A_g, B_g] = params_gen_res_relu(500, d);
   for trails_it = 1 : trials
     
     % lp
@@ -33,19 +28,15 @@ for T_gt_it = 1 : T_gt
     B_lp = inv(C);
     H = C * Y_st - X_st;
     A_unscaled = relulp2_layer1(X_st, H);
-    A_lp = rescale_layer1(X_train, H, A_unscaled);
-    Y_pred_lp = C \ (max(A_lp * X_stest, 0) + X_stest);
+    A_lp = rescale_layer1(X_st, H, A_unscaled);
+    Y_pred_lp = mean(C \ (max(A_lp * X_stest, 0) + X_stest));
     
-    A_errs_lp(i) = norm(A_lp - A_g) / norm(A_g);
-    B_errs_lp(i) = norm(B_lp - B_g) / norm(B_g);
     Y_errs_lp(i) = mean(vecnorm(Y_pred_lp - Y_stest) ./ vecnorm(Y_stest));
     
     % bp
 %     [A_bp, B_bp] = backprop2(X_train, Y_train, X_test, Y_test, 32, 1e-3, 1e-5, 256);
 %     Y_pred_bp = B_bp * (max(A_bp * X_test, 0) + X_test);
 %     
-%     A_errs_bp(i) = norm(A_bp - A_g) / norm(A_g);
-%     B_errs_bp(i) = norm(B_bp - B_g) / norm(B_g);
 %     Y_errs_bp(i) = mean(vecnorm(Y_pred_bp - Y_test) ./ vecnorm(Y_test));
     
     i = i + 1;
--- storecifar10.m
+++ storecifar10.m
@@ -30,21 +30,28 @@ test_label = labels;
 % Y in (d, N)
 % transfer Y into square matrix by duplicating d times
 
-X_train = normalize(double(transpose(train)));
+X_train = normalize(double(transpose(train)),2);
 Y_train = double(transpose(train_label));
-
-
-X_st = X_train(:,1:50);
-Y_st = Y_train(:,1:50);
-Y_st = repmat(Y_st,3072,1);
-
-X_test = normalize(double(transpose(test)));
+X_test = normalize(double(transpose(test)),2);
 Y_test = double(transpose(test_label));
 
-X_stest = X_test(:,1:10);
-Y_stest = Y_test(:,1:10);
-Y_stest = repmat(Y_stest,3072,1);
+%[d, N] = size(X_train);
+d = 400;
+n1 = 1000; n2 = 10;
 
+%smaller size
+X_st = X_train(1:d,1:n1);
+Y_st = Y_train(:,1:n1);
+Y_st = repmat(Y_st,d,1);
 %adding random noise to Y
+rn = unifrnd(-0.1,0.1,d,n1);
+Y_st = Y_st + rn;
+
+
+X_stest = X_test(1:d,1:n2);
+Y_stest = Y_test(:,1:n2);
+Y_stest = repmat(Y_stest,d,1);
+rn2 = unifrnd(-0.1,0.1,d,n2);
+Y_stest = Y_stest + rn2;
 
 save("cifar10data",'X_train','Y_train','X_test','Y_test','X_st','Y_st','X_stest','Y_stest');
